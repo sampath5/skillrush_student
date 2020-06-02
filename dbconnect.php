58e3c17646ffe6544fc3d757e984f0108f97aa19
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-02 17:40:53 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$url = parse_url(getenv("https://www.cleardb.com/database/details?id=59FB37C4CF0D8D262D52412313A05AA4"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
