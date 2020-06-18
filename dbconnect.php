f800dd6292ac8fa7a4ebd90f659c83c93399267c
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-18 16:59:32 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -6,7 +6,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 */
-$server = "database-1.cigyk57c3ye9.us-east-1.rds.amazonaws.com";
+$server = "skillrushdb.cigyk57c3ye9.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "skillrush";
 $db = "database-1";
