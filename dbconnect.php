a1c913c3ee9d78c8f0863b3b50157ba11c9c39cd
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-18 14:52:57 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,14 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+{heroku details}
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
+*/
+$server = "database-1.cigyk57c3ye9.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "skillrush";
+$db = "database-1";
 $conn = new mysqli($server, $username, $password, $db);
 ?>
