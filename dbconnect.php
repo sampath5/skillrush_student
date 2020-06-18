54e33b2c29217a771f17815bbee483b853b93de1
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-18 15:04:57 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -10,5 +10,9 @@ $server = "database-1.cigyk57c3ye9.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "skillrush";
 $db = "database-1";
-$conn = new mysqli($server, $username, $password, $db);
+if(!($conn = new mysqli($server, $username, $password, $db)))
+{
+     die(' connection failed ! --> '.mysqli_connect_error());
+}
+
 ?>
