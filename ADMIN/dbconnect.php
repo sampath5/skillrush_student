1122fe9e78cde8b8b68df4ecdb6d1079cced768d
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-19 10:30:18 +0530

    Update dbconnect.php

--- ADMIN/dbconnect.php
+++ ADMIN/dbconnect.php
@@ -1,10 +1,17 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
+*/
+$server = "skillrushdb1.cigyk57c3ye9.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "skillrush";
+$db = "vjit";
+if(!($conn = new mysqli($server, $username, $password, $db)))
+{
+     die(' connection failed ! --> '.mysqli_connect_error());
+}
 ?>
