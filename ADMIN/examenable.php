5c20fc40debb1cedb0dd8e23b73ea73649819556
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-06-20 14:28:39 +0530

    Update examenable.php

--- ADMIN/examenable.php
+++ ADMIN/examenable.php
@@ -5,43 +5,54 @@ date_default_timezone_set('Asia/Kolkata');
 if(isset($_GET['sno'])){
     $sno=$_GET['sno'];
     $changeto=$_GET['changeto'];
-    $query="UPDATE `list_of_exams` SET `Enable`=$changeto WHERE sno=$sno;";
-    if(mysqli_query($conn,$query)){
-        $sql="select exam_name from list_of_exams where sno=$sno;";
-        $res=mysqli_query($conn,$sql);
-        $row=mysqli_fetch_assoc($res);
-        $insert=0;
-        $exam=$row['exam_name'];
-        if($changeto=='1'){
-        $que="create table result_$exam(sno int PRIMARY KEY AUTO_INCREMENT ,rollno varchar(10) UNIQUE,Marks int,result varchar(10))";
-        $st=date("y-m-d H:i:s");
-        if(mysqli_query($conn,$que)){
-        
-                // echo"table result";
-                echo $st;
-                 $query2="INSERT INTO `examconducted`(`exam_name`, `starttime`, `result`) VALUES ('$exam','$st','NO')";
-                 if(mysqli_query($conn,$query2))
-                   $_SESSION['started']=$st;
-        }
-        // else{
-        //     echo"<script>alert('this exam is already conducted.To reconduct the exam delete the exam related data in database(examconducted table) and delete result table 
-        //         of this particular exam.Make sure u stored the data in excel sheet');</script>";
-        // }   
-        
-    }
-        //For disabling the exam
-    else{
-            $et=date("y-m-d H:i:s");
-            // echo"updated";
-            $updatequery="UPDATE `examconducted` SET `endtime`='$et' WHERE `exam_name`='$exam'";
-            if($_SESSION['started'])
-            {if(mysqli_query($conn,$updatequery))
-                unset($_SESSION['started']);
+    $examname_query="select exam_name from list_of_exams where sno=$sno";
+    $row=mysqli_fetch_assoc(mysqli_query($conn,$examname_query));
+    $exam=$row['exam_name'];
+
+    $sql_query="select * from examconducted where exam_name='$exam'";
+    //To enable
+    
+     if($changeto==1){
+        $res=mysqli_query($conn,$sql_query);
+            if(mysqli_num_rows($res)==0)
+            {
+                
+                $query="create table result_$exam(sno int PRIMARY KEY AUTO_INCREMENT ,rollno varchar(10) UNIQUE,Marks int,result varchar(10))";
+                if(mysqli_query($conn,$query))
+                {
+                    $que="UPDATE `list_of_exams` SET `Enable`=$changeto WHERE sno=$sno;";
+                    $st=date("y-m-d H:i:s");
+                    if(mysqli_query($conn,$que))
+                    {
+                        echo $st;
+                        $query2="INSERT INTO `examconducted`(`exam_name`, `starttime`, `result`) VALUES ('$exam','$st','NO')";
+                        if(mysqli_query($conn,$query2))
+                        {
+                            echo"Exam enabled";
+                        }
+                    } 
+                }
+                else{
+                    echo"Failed to enable exam.Unable to generate Result table or already existing ";
+                    //http_response_code(453);
+                }  
             }
-    }
-    }
-    else{
-        http_response_code(456);
+            else{
+                echo"Exam is already conducted.To reconduct the exam u have to delete result table and remove exam data from examconducted table";
+            }
+        }
+    //For disabling the exam
+    else
+    {
+        $et=date("y-m-d H:i:s");
+        $updatequery="UPDATE `examconducted` SET `endtime`='$et' WHERE `exam_name`='$exam'";
+        if(mysqli_query($conn,$updatequery))
+        {
+            $query="UPDATE `list_of_exams` SET `Enable`=$changeto WHERE sno=$sno;";
+            if(mysqli_query($conn,$query))
+                echo"$et Exam Disabled";
+        }
+        
     }
 }
 
